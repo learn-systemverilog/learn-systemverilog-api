b0594cc59506c060da36207564ad206339235de4
Icaro-Lima <icaro.lima@wildlifestudios.com>
2021-02-14 17:28:21 +0000

    Remove unused constants

--- transpiler/constants.go
+++ transpiler/constants.go
@@ -3,12 +3,6 @@ package transpiler
 const (
 	mainSVFileName        = "top.sv"
 	workSpaceTemplatePath = "workspace_template"
-	msgTypeInfo           = "info"
-	msgTypeWarning        = "warning"
-	msgTypeError          = "error"
-	msgTypeStdout         = "stdout"
-	msgTypeStderr         = "stderr"
-	msgTypeExit           = "exit"
 
 	logInternalSeverityTrace = "trace"
 	logInternalSeverityDebug = "debug"
