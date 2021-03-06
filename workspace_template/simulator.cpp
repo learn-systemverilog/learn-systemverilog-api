df2d3e9f3581acf167e4e75d42ac781d773cea98
Icaro-Lima <icarodantas.lima@gmail.com>
2021-03-06 18:49:18 -0300

    Remove unused variables

--- workspace_template/simulator.cpp
+++ workspace_template/simulator.cpp
@@ -13,31 +13,10 @@ string top_to_string()
 {
     stringstream buffer;
 
-    buffer << "{"
-           << "\"led\": " << +top->LED
-           << ", \"seg\": " << +top->SEG
-           << ", \"lcd_a\": " << +top->lcd_a
-           << ", \"lcd_b\": " << +top->lcd_b
-           << ", \"lcd_instruction\": " << +top->lcd_instruction;
+    buffer << "{";
 
-    buffer << ", \"lcd_registrador\": [" << +top->lcd_registrador[0];
-    for (int i = 1; i < 32; i++)
-    {
-        buffer << ", " << +top->lcd_registrador[i];
-    }
-    buffer << "]";
-
-    buffer << ", \"lcd_pc\": " << +top->lcd_pc
-           << ", \"lcd_src_a\": " << +top->lcd_SrcA
-           << ", \"lcd_src_b\": " << +top->lcd_SrcB
-           << ", \"lcd_alu_result\": " << +top->lcd_ALUResult
-           << ", \"lcd_result\": " << +top->lcd_Result
-           << ", \"lcd_write_data\": " << +top->lcd_WriteData
-           << ", \"lcd_read_data\": " << +top->lcd_ReadData
-           << ", \"lcd_mem_write\": " << +top->lcd_MemWrite
-           << ", \"lcd_branch\": " << +top->lcd_Branch
-           << ", \"lcd_memto_reg\": " << +top->lcd_MemtoReg
-           << ", \"lcd_reg_write\": " << +top->lcd_RegWrite;
+    buffer << "\"led\": " << +top->LED
+           << ", \"seg\": " << +top->SEG;
 
     buffer << "}";
 
